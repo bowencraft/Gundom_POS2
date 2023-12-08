a1dc8bebcc1fcab015083f27a81a333d7c3cee76
bellinimoon <98369237+bellinimoon@users.noreply.github.com>
2023-12-08 00:40:49 -0500

    changed scenes

--- Assets/Scripts/DontDestroyOnLoad.cs
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -1,14 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static DontDestroyOnLoad robot;
+    public bool refresh = false;
+
     void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if (robot == null)
+        {
+            robot = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            
+        }
     }
+    void LateUpdate()
+    {
+        if(!refresh && SceneManager.GetActiveScene().name != "Showcase")
+        {
+            ConfirmRefresh();
+        }
+
+        if (refresh && SceneManager.GetActiveScene().name == "Showcase")
+        {
+            Invoke("Refresh", 0);
+            refresh = false;
+        }
+    }
+    public void Refresh()
+    {
+        // restart
+        print("restart");
+        if (SceneManager.GetActiveScene().name != "Showcase")
+        {
+            Destroy(gameObject);
+            Instantiate(gameObject);
+        }
+    }
+
+    public void ConfirmRefresh()
+    {
+        refresh = true;
+    }
+
 
 
 }
